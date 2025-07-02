260eb226806b84b41e9393c308405b65db7a8b9a
Jun Luo <4catcode@gmail.com>
2025-07-02 15:10:11 +0800

    refactor(core): improved Stellar transaction signing interface for a more streamlined user experience.

--- core/src/apps/stellar/get_address.py
+++ core/src/apps/stellar/get_address.py
@@ -29,7 +29,7 @@ async def get_address(msg: StellarGetAddress, keychain: Keychain) -> StellarAddr
     if msg.show_display:
         from . import PATTERN, SLIP44_ID
 
-        coin = "XLM"
+        coin = "Stellar"
         await show_address(
             address,
             subtitle=TR.address__coin_address_template.format(coin),
